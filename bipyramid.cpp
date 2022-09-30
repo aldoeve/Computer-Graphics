f5ca50d63814ea61474c6fd59f0f228f3ea46ab3
aldoeve <96842012+aldoeve@users.noreply.github.com>
2022-09-30 00:17:32 -0500

    need to find a way apply the bottom half

--- bipyramid.cpp
+++ bipyramid.cpp
@@ -23,68 +23,105 @@ using namespace std;
 
 //This function generates the points for a pentagon and inserts them into a given array of arrays.
 void generatePentagon(float point[][3], int startingIndex, float radius){
-    float angleIncreament(360.0f/5.0f);
-    angleIncreament *= M_PI / 180.0f;
-    float angle(0.0f);
-    for (int i(0); i < 5; ++i, ++startingIndex) {
-        point[startingIndex][0] = (float)(radius * cos(angle));
-        point[startingIndex][1] = (float)(radius * sin(angle));
-        angle += angleIncreament;
-    }
-    return;
+   float angleIncreament(360.0f/5.0f);
+   angleIncreament *= M_PI / 180.0f;
+   float angle(0.0f);
+   for (int i(0); i < 5; ++i, ++startingIndex) {
+      point[startingIndex][0] = (float)(radius * cos(angle));
+      point[startingIndex][2] = (float)(radius * sin(angle));
+      angle += angleIncreament;
+   }
+   return;
 }
 
 void display() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glEnable(GL_DEPTH_TEST);
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f);
 
-    float point[][3] = {
-        {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f},
-        {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}
-    };
-    generatePentagon(point, 0, 0.5f);
-    glRotatef(50.0f, 1.0f, 1.0f, 0.0f);
-    //glRotatef(50.0f, 1.0f, 1.0f, 1.0f);
+   float point[][3] = {
+      {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f},
+      {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.7f, 0.0f}
+   };
+   generatePentagon(point, 0, 0.5f);
+   glTranslatef(-point[1][0], -point[1][1], -point[1][2]);
+   glRotatef(35.0f, 1.0f, 1.0f, 1.0f);
+   
+   //Top half base used to guide where the sides would be placed
+   glBegin(GL_TRIANGLE_FAN);
+      glColor3f(0.0f, 0.0f, 1.0f);
+      glVertex3fv(point[0]);
+      glVertex3fv(point[1]);
+      glVertex3fv(point[2]);
+      glVertex3fv(point[3]);
+      glVertex3fv(point[4]);
+   glEnd();
 
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3fv(point[0]);
-    glVertex3fv(point[1]);
-    glVertex3fv(point[2]);
-    glVertex3fv(point[3]);
-    glVertex3fv(point[4]);
-    glEnd();
+   //Top half sides of the shape
+   glBegin(GL_TRIANGLES);
+      glColor3f(1.0f, 0.0f, 0.0f);
+      glVertex3fv(point[0]);
+      glVertex3fv(point[5]);
+      glVertex3fv(point[4]);
+      glColor3f(0.0f, 1.0f, 0.0f);
+      glVertex3fv(point[4]);
+      glVertex3fv(point[5]);
+      glVertex3fv(point[3]);
+      glColor3f(0.8f, 0.4f, 0.0f);
+      glVertex3fv(point[1]);
+      glVertex3fv(point[5]);
+      glVertex3fv(point[0]);
+      glColor3f(0.0f, 0.5f, 0.7f);
+      glVertex3fv(point[2]);
+      glVertex3fv(point[5]);
+      glVertex3fv(point[1]);
+      glColor3f(0.3f, 0.6f, 0.3f);
+      glVertex3fv(point[3]);
+      glVertex3fv(point[5]);
+      glVertex3fv(point[2]);
+   glEnd();
 
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3fv(point[0]);
-    glVertex3fv(point[5]);
-    glVertex3fv(point[4]);
-    glVertex3fv(point[1]);
-    glVertex3fv(point[5]);
-    glVertex3fv(point[0]);
-    glVertex3fv(point[2]);
-    glVertex3fv(point[5]);
-    glVertex3fv(point[1]);
-    glVertex3fv(point[3]);
-    glVertex3fv(point[5]);
-    glVertex3fv(point[2]);
-    glEnd();
+   glLoadIdentity();
+   glOrtho(-2.0f, 2.0f, -2.0f, 2.0f, -2.0f, 2.0f);
+   glRotatef(305.0f, 1.0f, 1.0f, 1.0f);
 
-    glFlush();
+   //bottom half of the sides
+   //glBegin(GL_TRIANGLES);
+   //   glColor3f(1.0f, 0.0f, 0.0f);
+   //   glVertex3fv(point[0]);
+   //   glVertex3fv(point[5]);
+   //   glVertex3fv(point[4]);
+   //   glColor3f(0.0f, 1.0f, 0.0f);
+   //   glVertex3fv(point[4]);
+   //   glVertex3fv(point[5]);
+   //   glVertex3fv(point[3]);
+   //   glColor3f(0.8f, 0.4f, 0.0f);
+   //   glVertex3fv(point[1]);
+   //   glVertex3fv(point[5]);
+   //   glVertex3fv(point[0]);
+   //   glColor3f(0.0f, 0.5f, 0.7f);
+   //   glVertex3fv(point[2]);
+   //   glVertex3fv(point[5]);
+   //   glVertex3fv(point[1]);
+   //   glColor3f(0.3f, 0.6f, 0.3f);
+   //   glVertex3fv(point[3]);
+   //   glVertex3fv(point[5]);
+   //   glVertex3fv(point[2]);
+   //glEnd();
+
+   glFlush();
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitWindowSize(640, 640);
-    glutInitWindowPosition(50, 50);
-    glutCreateWindow("Bipyramid");
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
+   glutInit(&argc, argv);
+   glutInitWindowSize(640, 640);
+   glutInitWindowPosition(50, 50);
+   glutCreateWindow("Bipyramid");
+   glutDisplayFunc(display);
+   glutMainLoop();
+   return 0;
 }
