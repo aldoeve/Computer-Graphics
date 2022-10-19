2eb58952af6410040d0090754dcaf82c75cf62b2
aldoeve <96842012+aldoeve@users.noreply.github.com>
2022-10-18 22:37:24 -0500

    .

--- scene.cpp
+++ scene.cpp
@@ -18,6 +18,8 @@ void init(){
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHT1);
     glEnable(GL_NORMALIZE);
+    float point[] = {0.1f, 0.6f, -0.25f};
+    glLightfv(GL_LIGHT0, GL_POSITION, point);
     glutMouseFunc(trackballMouseFunction);
     glutMotionFunc(trackballMotionFunction);
 }
@@ -105,26 +107,33 @@ void cylinder(const int sides){
 }
 
 void cube(){
-    glTranslatef(0.0f, 1.7f, 0.0f);
-    wall();
+    glPushMatrix();
+        glRotatef(-90, 0.0f, 0.0f, -1.0f);
+        wall();
     glPopMatrix();
-    glTranslatef(0.0f, -1.7f, 0.0f);
-    glRotatef(90, 0.0f, 0.0f, 1.0f);
-    wall();
+    glPushMatrix();
+        glTranslatef(0.0f, 1.7f, 0.0f);
+        wall();
     glPopMatrix();
-    glTranslatef(0.0f, 0.0f, 1.7f);
-    glRotatef(90, 1.0f, 0.0f, 0.0f);
-    wall();
+    glPushMatrix();
+        glTranslatef(1.7f, 1.7f, 0.0f);
+        glRotatef(90, 0.0f, 0.0f, -1.0f);
+        wall();
     glPopMatrix();
+    glPushMatrix();
+        glTranslatef(0.0f, 0.0f, 1.7f);
+        glRotatef(180, 1.0f, 0.0f, 0.0f);
+        wall();
     glPopMatrix();
+    glPushMatrix();
+        glRotatef(-90, 1.0f, 0.0f, 0.0f);
+        wall();
     glPopMatrix();
-    glTranslatef(0.0f, 0.0f, 1.7f);
-    glRotatef(90, 1.0f, 0.0f, 0.0f);
-    wall();
+    glPushMatrix();
+        glTranslatef(0.0f, 1.7f, 1.7f);
+        glRotatef(-270, 1.0f, 0.0f, 0.0f);
+        wall();
     glPopMatrix();
-    glPopMatrix();
-    glTranslatef(0.0f, 1.1f, 0.0f);
-    wall();
 }
 
 void display(){
@@ -134,34 +143,48 @@ void display(){
     cameraSetLimits(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
     cameraApply();
 
-    float cube_color[] = { 0.7f, 0.0f, 0.7f, 1.0f };
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, cube_color);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, cube_color);
-    glMaterialfv(GL_FRONT, GL_AMBIENT, cube_color);
+    float cubeColor0[] = {0.9f, 0.8f, 0.0f, 0.3f};
+    float cubeColor1[] = {0.0f, 0.4f, 0.4f, 1.0f};
+    float cubeColor2[] = {0.7f, 0.4f, 0.1f, 1.0f};
+    float moonColor[] = {0.96f, 0.93f, 0.84f};
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, cubeColor1);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, cubeColor0);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, cubeColor0);
     glMaterialf(GL_FRONT, GL_SHININESS, 50.0F);
-
-    cube();
+    glMaterialf(GL_BACK, GL_SHININESS, 10.0f);
+    glMaterialfv(GL_BACK, GL_DIFFUSE, cubeColor2);
+    glMaterialfv(GL_BACK, GL_SPECULAR, moonColor);
+    glMaterialfv(GL_BACK, GL_AMBIENT, moonColor);
 
     //room walls
-    //glTranslatef(-0.85f, -0.85f, -0.85f);
-    //wall();
-    //glTranslatef(0.0f, 1.7f, 0.0f);
-    //glRotatef(270, 0.0f, 0.0f, 1.0f);
-    //wall();
-    //glPopMatrix();
-    //glPopMatrix();
-    //glTranslatef(0.0f, 0.0f, 1.7f);
-    //glRotatef(270, 1.0f, 0.0f, 0.0f);
-    //wall();
-    //glPopMatrix();
-    //glPopMatrix();
-    //glPopMatrix();
-//
-    ////lightbulb string
-    //glTranslatef(0.0f, 1.0f, 1.0f);
-    //glRotatef(90, 0.0f, 1.0f, 0.0f);
-    //glScalef(0.01f, 0.01f, 0.2f);
-    //cylinder(50);
+    glPushMatrix();
+        glTranslatef(-0.85f, -0.85f, -0.85f);
+        wall();
+        glTranslatef(0.0f, 1.7f, 0.0f);
+        glRotatef(270, 0.0f, 0.0f, 1.0f);
+        wall();
+        glTranslatef(0.0f, 0.0f, 1.7f);
+        glRotatef(270, 1.0f, 0.0f, 0.0f);
+        wall();
+    //lightbulb string
+        glTranslatef(0.0f, 1.0f, 1.0f);
+        glRotatef(90, 0.0f, 1.0f, 0.0f);
+        glScalef(0.01f, 0.01f, 0.2f);
+        cylinder(50);
+    glPopMatrix();
+    
+    //bulb
+    float cube_color1[] = {0.9f, 0.8f, 0.0f, 0.3f};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, cube_color1);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, cube_color1);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, cube_color1);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50.0F);
+    glPushMatrix();
+        glTranslatef(0.08f, 0.56f, -0.22f);
+        glRotatef(35, 1.0f, 0.0f, 0.0f);
+        glScalef(0.07f, 0.07f, 0.07f);
+        cube();
+    glPopMatrix();
 
     glFlush();
 }
